8473e1f2a97c11fbdde7b3c8dbc4231252e83ba5
bhaveshyadav24 <144918265+bhaveshyadav24@users.noreply.github.com>
2024-01-07 09:53:15 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
