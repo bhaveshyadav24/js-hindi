80a4a1ebac6b7283f9b609ff9d3c6eeee5f978c3
bhaveshyadav24 <144918265+bhaveshyadav24@users.noreply.github.com>
2024-01-07 09:30:56 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553 
+let accountEmail = "bhavesh@google.com"
+var accountPassword = "12345"
+var accountCity = "indore"
+
+accountCity = "bhopal"
+accountEmail = "rahul@1234"
+accountPassword  = "98989898"
+
+console.table([accountCity,accountEmail,accountPassword]);
+
+
+/*prefer not to use var
+ because of issue in block scope and function scope
+ */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; // use for treat all js code as newer version
+
+// alert (3+3) // we are using node js not a browser
